457f4aa3924340af650a442356560a78426c334a
ALGIS DAVID <david.algis@etu.unistra.fr>
2020-05-14 23:44:50 +0200

    add some files

---
+++ Assets/Script/Knowledge.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Knowledge : MonoBehaviour
+{
+
+
+}
+
+
+public class Password : Knowledge
+{
+    [SerializeField]
+    private GameObject m_elementAssociated;
+
+    [SerializeField]
+    private string m_password;
+
+}
